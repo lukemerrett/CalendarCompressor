c7368bd0972392dd56490d3aa95bc601c648f9d3
Luke Merrett <lukeamerrett@gmail.com>
2018-12-16 20:25:48 +0000

    Scaffolding compressor service

---
+++ CalendarCompressor/Services/Compressor/CompressorService.cs
@@ -0,0 +1,14 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+
+namespace CalendarCompressor.Services.Compressor
+{
+    public class CompressorService : ICompressorService
+    {
+        public IList<Event> CompressWeeksEvents(IList<Event> originalEvents)
+        {
+            throw new NotImplementedException("Core compression logic still to be written");
+        }
+    }
+}
---
+++ CalendarCompressor/Services/Compressor/ICompressorService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Google.Apis.Calendar.v3.Data;
+
+namespace CalendarCompressor.Services.Compressor
+{
+    public interface ICompressorService
+    {
+        IList<Event> CompressWeeksEvents(IList<Event> originalEvents);
+    }
+}
